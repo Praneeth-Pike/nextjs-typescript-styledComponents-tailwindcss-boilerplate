ea15bbb601f3b78a11dd853ccd7c93e0c6cc5535
Praneeth-Pike <praneeth@clearcut.design>
2021-05-07 02:11:27 +0530

    Boilerplate done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import Image from "next/image"
 import tw from "twin.macro"
+import Row from "../components/layouts/Row"
 
 const Home = () => {
 	return (
@@ -14,25 +15,10 @@ const Home = () => {
 			</Head>
 
 			<main>
-				<h1 tw="text-6xl font-bold">Hello!</h1>
+				<Row>
+					<h1 tw="text-6xl font-bold">Hello!</h1>
+				</Row>
 			</main>
-
-			<footer>
-				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer">
-					Powered by{" "}
-					<span>
-						<Image
-							src="/vercel.svg"
-							alt="Vercel Logo"
-							width={72}
-							height={16}
-						/>
-					</span>
-				</a>
-			</footer>
 		</div>
 	)
 }
